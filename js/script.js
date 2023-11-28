504e3d6e8ec08a96debf565a6050696a26b95c33
ohiagosouza <ohiagossouza@gmail.com>
2023-11-28 00:54:14 -0300

    job-title typing effect

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
+let i = 0;
+const title = "Engenheiro da Computação & Desenvolvedor Full-Stack";
+const speed = 75;
 
+window.onload = function typeWriter() {
+  if (i < title.length) {
+    document.getElementById("job-title").innerHTML += title.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+};
